431eeb4846824d12c865168429163e4380fbf649
devPrakash7 <psamantaray77@gmail.com>
2023-07-28 15:23:34 +0530

    dhvdd

--- Driver_modules/controller/driver.controller.js
+++ Driver_modules/controller/driver.controller.js
@@ -8,25 +8,16 @@ const jwt = require("jsonwebtoken")
 const ExcelJs = require("exceljs");
 const fs = require("fs");
 const driver = require("../../models/driver.model");
-// const {totalEarningbyDriver} = require("../../middleware/earning.system")
 
 
 
-exports.driver_signup = async (req, res) => {
+exports.signup = async (req, res) => {
 
   try {
     
     const reqBody = req.body;
     const {driver_name , driver_email , driver_mobile_number , Aadhar_card_number , pan_card_number,brand,truck_type,driving_licence,account_number,ifsc_code} = reqBody;
-    // let file = req.file;
-    // reqBody.driver_img = file.originalname;
     reqBody.driverId = generate_Id();
-    // const pickup_location = await geocoder.geocode(reqBody.driver_current_location);
-
-    //  pickup_location.map((item) => {
-    //    reqBody.driver_lat = item.latitude;
-    //    reqBody.driver_long = item.longitude;
-    // });
 
     reqBody.authTokens = await jwt.sign(
       {
--- Driver_modules/routes/driver.rout.js
+++ Driver_modules/routes/driver.rout.js
@@ -1,6 +1,5 @@
 var express = require("express");
-const {
-  driver_signup,
+const {signup,
   login,
   logout,
   generate_auth_tokens,
@@ -10,14 +9,12 @@ const {
   export_driver_data_into_excel_file,
   driver_file_export_into_csv_file,
 } = require("../controller/driver.controller");
-const {
-  driver_validator,
-  validation_result,
-} = require("../../validation/driver.validator");
+
 var router = express.Router();
 const { driver_authenticate } = require("../../middleware/authenticate");
 
-router.post("/driver_signup", driver_signup);
+
+router.post("/signup", signup);
 router.post("/login", login);
 router.get("/logout", driver_authenticate, logout);
 router.get("/auth_tokens/:refresh_tokens", generate_auth_tokens);
