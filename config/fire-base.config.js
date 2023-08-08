24790dbb9c0edbe493b9dabaef0ccb9bbfb936f7
user <psamantaray77@gmail.com>
2023-08-08 18:37:13 +0530

    chgdd

--- config/fire-base.config.js
+++
@@ -1,7 +0,0 @@
-const admin = require('firebase-admin');
-
-const serviceAccount = require('../keys/fire-base.service');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
--- keys/keys.js
+++
@@ -1,5 +0,0 @@
-if(process.env.ENV === 'production'){
-    module.exports = require('./production.keys');
-}else{
-    module.exports = require('./development.keys');
-}
