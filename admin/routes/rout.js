6bf2bb329b1d23e1804275b477bd87ceb6377a8d
prakash samanatartay <psamantaray77@gmail.com>
2023-10-07 14:21:40 +0530

    changes for

---
+++ admin/routes/rout.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const adminAuthenticate = require('../../middleware/admin.middleware')
+const {
+  login,
+  logout,
+  signUp
+} = require('../controllers/admin.controller')
+
+
+router.post('/signup' , signUp)
+router.post('/login',  login)
+router.get('/logout', adminAuthenticate, logout)
+
+
+
+module.exports = router;
